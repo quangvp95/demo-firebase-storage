cf23729d6b0d7f3c65579bde4a0cb188923f1795
quangnh <quangnh@coccoc.com>
2020-11-19 17:09:20 +0700

    Init

---
+++ app/src/main/java/chat/Chat.java
@@ -0,0 +1,37 @@
+package chat;
+
+/**
+ * @author greg
+ * @since 6/21/13
+ */
+public class Chat {
+
+    private String message;
+    private String author;
+
+    // Required default constructor for Firebase object mapping
+    @SuppressWarnings("unused")
+    private Chat() {
+    }
+
+    Chat(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Chat{" +
+                "message='" + message + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
